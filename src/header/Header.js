79da9444226c9325c85b1b6b94a8edd44757edc0
wombat1972 <53923403+wombat1972@users.noreply.github.com>
2021-05-02 17:50:59 -0400

    header started

---
+++ src/header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+	return (
+		<div className='headBacker'>
+			<div className='headWrap'>
+				<div className='contentWrap'></div>
+			</div>
+		</div>
+	);
+}
+
+export default Header;
