fca0dcec17dc81217b34f0364dec1ba0f5f69184
wombat1972 <53923403+wombat1972@users.noreply.github.com>
2021-05-02 19:09:08 -0400

    header close to complete

--- src/header/Header.js
+++ src/header/Header.js
@@ -4,7 +4,20 @@ function Header() {
 	return (
 		<div className='headBacker'>
 			<div className='headWrap'>
-				<div className='contentWrap'></div>
+				<div className='contentWrap'>
+					<div className='broodin'></div>
+					<div className='welcomeText'>
+						<h1>WELCOME TO THE HOME OF PANCAKE CAT</h1>
+						<h2>
+							Currently Pancake Cat is working as the campaign manager for
+						</h2>
+						<h2>The 2021 Broodin for CSM campaign</h2>
+						<h3>
+							Pancake Cat couldn't be more pleased if you would take a look at
+							the details below.
+						</h3>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
