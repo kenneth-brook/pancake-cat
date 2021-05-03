9e7cdfcf14ecae17ee9d4d0a579b187d078de810
wombat1972 <53923403+wombat1972@users.noreply.github.com>
2021-05-02 22:23:55 -0400

    nav bar created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./header/Header";
+import Nav from "./nav/Nav";
 
 function App() {
 	return (
 		<div className='App'>
 			<Header />
+			<Nav />
 		</div>
 	);
 }
---
+++ src/nav/Nav.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Nav() {
+	return (
+		<div className='navWrap'>
+			<div className='navLeft'></div>
+			<div className='center1'>
+				<div className='center2'>
+					<div className='center3'></div>
+				</div>
+			</div>
+			<div className='navRight'></div>
+		</div>
+	);
+}
+
+export default Nav;
