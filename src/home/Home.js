86f5489c839fd237db5b69a131d19510aa978d70
wombat1972 <53923403+wombat1972@users.noreply.github.com>
2021-05-03 18:52:33 -0400

    revised body content

--- src/home/Home.js
+++ src/home/Home.js
@@ -9,30 +9,89 @@ function Home() {
 				source as possible and in the most logical way possible.
 			</p>
 			<p>
-				We want more smaller groups in EvE rather than a solid blue doughnut of
-				mega coalitions. One of the ways this can be incentivized is to develop
-				and publish corp / alliance tools such as an ESI check program along
-				with SOV and structure management tools. This method is designed to
-				enable smaller, newer groups who would otherwise lack the tools needed
-				to effectively administrate a community in EvE.
+				You will often hear me say things such as “a pessimist says no one
+				should have that… an optimist says everyone should be able to have
+				that”. This is because I have a less common outlook on gaming in general
+				and a deliberate approach to my problem-solving process in EvE. My
+				background in the military has made me quite wary of the “good idea
+				fairy” approach and the damage that a stupid idea can cause to the
+				average person’s caught in a bad situation.
 			</p>
 			<p>
-				Another concept is to develop, at the CSM and CCP levels, a set of
-				specific and measurable criteria which “New Player Friendly” groups can
-				aspire to. Those that meet the criteria set forth would be included in a
-				list which randomizes the “New player Friendly” groups and is unsortable
-				to avoid any size or alphabetical preference. This list should function
-				more in line with the structure browser in design with shaded or
-				illuminated icons for what the group focuses on and include basic
-				information on the groups identity.
+				My background in EvE includes years in leadership of a small industrial
+				Corp. The last year has been spent leading and developing Karmafleet
+				University, a highsec teaching, and training division of Karmafleet. My
+				experience in being immersed in large and small groups and having lived
+				in multiple areas of the game over the last half a decade leaves me with
+				some unique perspectives on the problems facing EvE.
+			</p>
+			<ol>
+				<li>
+					<p>
+						1. As a community, I would venture to say that we want smaller
+						groups in EvE rather than a solid blue doughnut of mega coalitions.
+						One way this can be incentivized is to develop and publish corp /
+						alliance tools such as an ESI check program and SOV and structure
+						management tools at the CCP level. This method is designed to enable
+						smaller, newer groups who would otherwise lack the tools needed to
+						administrate a community in EvE effectively.{" "}
+					</p>
+				</li>
+				<li>
+					<p>
+						2. An often-debated problem but rarely approached is the difficulty
+						of connecting new players to the EvE community. My approach would be
+						to develop, at the CSM and CCP levels, a set of specific and
+						measurable criteria which “New Player Friendly” groups can aspire
+						to. Those that meet the requirements set forth would be included in
+						a list that randomizes the “New Player Friendly” groups and is
+						unsortable to avoid any size or alphabetical preference. This list
+						should function more in line with the structure browser in design
+						with shaded or illuminated icons. The group focuses on and includes
+						basic information on the group's identity.
+					</p>
+					<p>
+						The list should be put forth front during the initial introduction
+						to the game to get new players connected rapidly with the community.
+						This would allow CCP to satisfy their desire to tinker with the New
+						Player Experience and incentivize groups who want to participate in
+						treating new players well and helping them get a footing in-game
+						rather than functioning as a meat grinder.
+					</p>
+				</li>
+				<li>
+					<p>
+						3. There are always questions and concerns about the EvE economy and
+						general gameplay experience. My viewpoint on this is simple… Quit
+						throwing crap ideas at the wall and hoping that something sticks.
+						Scarcity and the industry changes are a sham, a complete knee-jerk
+						reaction to a problem created by the introduction of Excavator
+						Drones into the game. Belts, anomalies, moons, mineral distribution,
+						blueprint, and response changes, and the widespread drought of
+						materials in the game have all been forcefully tinkered with when
+						NONE were the initiator of the market upheaval, to begin with.
+					</p>
+					<p>
+						Remove excavators from the game, refund their value by converting
+						the existing drones into an appropriate number of augmented or
+						harvester mining drones, and then roll back every unnecessary and
+						harmful change made during the scarcity era.
+					</p>
+				</li>
+			</ol>
+			<p>
+				Today, this game exists as the greatest sandbox MMO in existence,
+				holding several Guinness Records and bringing thousands together in
+				common interests and conflict. Work with that fact, administrate and
+				play the game as it is today, not what you wish it were or your desires
+				from a decade past. EvE is not great because of game designers sitting
+				in a backroom writing lore; it is excellent because players make the
+				stories that fill volumes of text and several books.
 			</p>
 			<p>
-				The list should be put forth front during the initial introduction to
-				the game in order to get new players connected rapidly with the
-				community. This would allow CCP to satisfy their desire to tinker with
-				the New Player Experience and also incentivize groups who want to
-				participate to treat new players well and help them to get a footing in
-				game rather than functioning as a meat grinder.
+				The best days of New Eden are not behind us if we are diligent as a
+				community to ensure its future is bright and productive, with many wars
+				and many more stories to be made along the path.
 			</p>
 		</div>
 	);
