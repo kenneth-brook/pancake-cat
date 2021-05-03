83fb36359079c3c4584a48c0aae8de44ce79ac97
wombat1972 <53923403+wombat1972@users.noreply.github.com>
2021-05-02 22:50:00 -0400

    home body set

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./header/Header";
 import Nav from "./nav/Nav";
+import Home from "./home/Home";
 
 function App() {
 	return (
 		<div className='App'>
 			<Header />
 			<Nav />
+			<Home />
 		</div>
 	);
 }
---
+++ src/home/Home.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+function Home() {
+	return (
+		<div className='homeWrap'>
+			<h1>Broodin's Vision</h1>
+			<p>
+				My platform for CSM is simple, solve problems at or as close to their
+				source as possible and in the most logical way possible.
+			</p>
+			<p>
+				We want more smaller groups in EvE rather than a solid blue doughnut of
+				mega coalitions. One of the ways this can be incentivized is to develop
+				and publish corp / alliance tools such as an ESI check program along
+				with SOV and structure management tools. This method is designed to
+				enable smaller, newer groups who would otherwise lack the tools needed
+				to effectively administrate a community in EvE.
+			</p>
+			<p>
+				Another concept is to develop, at the CSM and CCP levels, a set of
+				specific and measurable criteria which “New Player Friendly” groups can
+				aspire to. Those that meet the criteria set forth would be included in a
+				list which randomizes the “New player Friendly” groups and is unsortable
+				to avoid any size or alphabetical preference. This list should function
+				more in line with the structure browser in design with shaded or
+				illuminated icons for what the group focuses on and include basic
+				information on the groups identity.
+			</p>
+			<p>
+				The list should be put forth front during the initial introduction to
+				the game in order to get new players connected rapidly with the
+				community. This would allow CCP to satisfy their desire to tinker with
+				the New Player Experience and also incentivize groups who want to
+				participate to treat new players well and help them to get a footing in
+				game rather than functioning as a meat grinder.
+			</p>
+		</div>
+	);
+}
+
+export default Home;
