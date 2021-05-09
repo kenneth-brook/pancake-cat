682cb560b4704a75d9af59718352e6bccae36b7a
wombat1972 <53923403+wombat1972@users.noreply.github.com>
2021-05-09 19:58:15 -0400

    updated footer

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -4,6 +4,9 @@ function Footer() {
 	return (
 		<div className='footerWrap'>
 			<h6>
+				Pancakecat.com <span>&#169;</span> 2021
+			</h6>
+			<h6>
 				Broodin for CSM and Pancakecat.com proudly brought to you by Kennetic
 				Concepts <span>&#169;</span> 2012-2021
 			</h6>
