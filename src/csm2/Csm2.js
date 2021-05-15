113ebdda30ffd615afa0b5d83aa4e9a2374d6627
wombat1972 <53923403+wombat1972@users.noreply.github.com>
2021-05-15 15:30:56 -0400

    col2 content started

--- src/csm2/Csm2.js
+++ src/csm2/Csm2.js
@@ -6,6 +6,41 @@ function Csm2() {
 			<div className='infoHead'>
 				<h3>Pancake Cat answers your questions.</h3>
 			</div>
+			<div className='questionWrap'>
+				<div className='questionHead'>
+					<h4>What is EvE Online?</h4>
+				</div>
+				<div className='questionBody'>
+					<p>
+						Eve Online is a space-based, persistent world massively multiplayer
+						online role-playing game developed and published by CCP Games.
+						Players of Eve Online can participate in a number of in-game
+						professions and activities, including mining, piracy, manufacturing,
+						trading, exploration, and combat.
+					</p>
+				</div>
+				<div className='questionHead'>
+					<h4>What is a CSM?</h4>
+				</div>
+				<div className='questionBody'>
+					<p>
+						The Council of Stellar Management is a player advocacy group,
+						consisting of 10 members democratically elected by the players to
+						advise and assist CCP in the continuous development of EVE Online.
+						The CSM brings focused and structured feedback from the community to
+						CCP and represents their views and interests.
+					</p>
+					<p>
+						The role of a CSM member is to represent the players in the ongoing
+						development of EVE Online by providing analysis, sharing
+						suggestions, and giving feedback based on the ongoing community
+						discussions and concerns to CCP. As an elected representative, a CSM
+						member will have clear communication paths with CCP and a chance to
+						influence the development of EVE Online many times during their
+						term.
+					</p>
+				</div>
+			</div>
 		</div>
 	);
 }
